9d1bc128cd473104663614fe4bdb9147e328f204
safahasnaoui22 <safadkhili22@gmail.com>
2026-07-04 10:05:05 +0100

    updated pushnotif

--- src/components/PushNotificationManager.tsx
+++ src/components/PushNotificationManager.tsx
@@ -38,7 +38,7 @@ export function PushNotificationManager() {
 
         const sub = await reg.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(vapidKey!),
+          applicationServerKey: urlBase64ToUint8Array(vapidKey!) as BufferSource,
         });
 
         await save(sub);
